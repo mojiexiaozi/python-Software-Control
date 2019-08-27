af7213f27eafe769d113c499ad5dc984386894f8
linyilin <15651838825@163.com>
2019-08-27 16:08:43 +0800

    software init change

---
+++ not_use/test_pytest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     pytest_learn
+   Description :
+   Author :
+   date：          2019/8/23
+-------------------------------------------------
+   Change Activity:
+                   2019/8/23:
+-------------------------------------------------
+"""
+__author__ = 'Lyl'
+
+import pytest
+
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
+
+
+if __name__ == '__main__':
+    pytest.main()
