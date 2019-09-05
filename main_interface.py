d548a7e68de2ee48c3b47d2e5f09d2421f3f281d
linyilin <15651838825@163.com>
2019-09-05 14:47:20 +0800

    change interface to pyqt5

---
+++ main_interface.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+----------------------------------------------------
+@Project      :    CASI003
+@File         :   main_interface.py
+@Time         :   2019/9/2 9:09
+@Author       :   kimi
+@Version      :   1.0
+@Contact      :   15651838825@163.com
+@License      :   (C)Copyright 2018-2019, CASI
+@Desc         :   None
+-----------------------------------------------------
+"""
+
+
+# ---------------------------------------------------
+# module import
+
+
+class Example(object):
+    def __init__(self):
+        # do something
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='CASI003', version='1.0', packages=[''], url='', license='(C)Copyright 2018-2019, CASI', author='linyilin',
+    author_email='15651838825', description='for keyboard and mouse event record and p;ayback')
